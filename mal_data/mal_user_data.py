8d5c0ca65062779f33c289a50c11de5fcce0eedb
Maroyan Putros <maroyanputros@Maroyans-MacBook-Air.local>
2025-08-27 12:40:47 -0400

    flattend the json to have only the title and the score of a users list, exported to user_anime_list

--- mal_data/mal_user_data.py
+++ mal_data/mal_user_data.py
@@ -1,6 +1,10 @@
 import requests
 import json
+import pandas as pd
 
+####################################################
+#  this script fetches the user's anime list and saves it to a CSV file 
+####################################################
 with open('token.json', 'r') as f:
     token_data = json.load(f)
 
@@ -10,8 +14,7 @@ url = "https://api.myanimelist.net/v2/users/@me/animelist"
 
 # want to grab only anime that has been finished and sorted by scores descending
 headers = {
-    "status" : "completed", 
-    "sort": "list_score",
+    'fields': 'list_status, score', 
     "limit": 500,  # max limit is 1000
 }
 
@@ -19,7 +22,18 @@ response = requests.get(url, headers={"Authorization": f"Bearer {access_token}"}
 
 if response.status_code == 200:
     data = response.json()
-    print(json.dumps(data, indent=2))  # pretty-print the result
+    # print(json.dumps(data, indent=2))  # pretty-print the result
 else:
     print(f"Error: {response.status_code}")
     print(response.text)
+
+
+anime_list = []
+for entry in data['data']:
+    anime_list.append({
+        "title": entry["node"]["title"],
+        "score": entry["list_status"]["score"]
+    })
+
+df = pd.DataFrame(anime_list)
+df.to_csv("user_anime_list.csv", index=False)
